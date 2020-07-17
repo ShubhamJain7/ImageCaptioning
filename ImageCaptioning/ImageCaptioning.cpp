472953e584d2e1a941ca0a843a9464867dcf4fae
Shubham <shubhamdjain7@gmail.com>
2020-07-17 16:39:13 +0530

    Inferencing fixes

--- ImageCaptioning/ImageCaptioning.cpp
+++ ImageCaptioning/ImageCaptioning.cpp
@@ -23,7 +23,7 @@ Mat preprocess_image(Mat image) {
 
     Mat image_resized;
     // resize image to (224x224) to fit model input dimensions
-    resize(image, image_resized, Size(224, 224));
+    resize(image, image_resized, Size(224, 224), INTER_LANCZOS4);
 
     // normalize image (values between 0-1)
     Mat image_float;
@@ -74,6 +74,7 @@ int main()
     // get processed image
     Mat image = preprocess_image(im);
 
+    
     // create ONNX env and sessionOptions objects
     Ort::Env env(ORT_LOGGING_LEVEL_WARNING, "test");
     Ort::SessionOptions session_options;
@@ -102,6 +103,12 @@ int main()
     // pass inputs through model and get output
     auto encoder_outs = encoder_session.Run(Ort::RunOptions{ nullptr }, encoder_input_names, &encoder_input_tensor, 1, encoder_output_names, 1);
     auto features = encoder_outs[0].GetTensorMutableData<float>();
+    
+    float features_data[256];
+    size_t features_len = encoder_outs[0].GetTensorTypeAndShapeInfo().GetElementCount();
+    for (size_t i = 0; i < features_len; i++) {
+        features_data[i] = features[i];
+    }
 
     /* --------------------------------------------------- Decoder ---------------------------------------------------------------- */
 
@@ -121,10 +128,17 @@ int main()
     // create input tensor
     auto decoder_memory_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
     size_t decoder_input_tensor_size = 1 * 256;
-    Ort::Value decoder_input_tensor = Ort::Value::CreateTensor<float>(decoder_memory_info, reinterpret_cast<float*>(features), decoder_input_tensor_size, 
+    Ort::Value decoder_input_tensor = Ort::Value::CreateTensor<float>(decoder_memory_info, reinterpret_cast<float*>(features_data), decoder_input_tensor_size, 
                                                                       decoder_input_node_dims.data(), 2);
 
     // pass inputs through model and get output
     auto decoder_outs = decoder_session.Run(Ort::RunOptions{ nullptr }, decoder_input_names, &decoder_input_tensor, 1, decoder_output_names, 1);
-    auto sample_ids = decoder_outs[0].GetTensorMutableData<float>();
+    auto sample_ids = decoder_outs[0].GetTensorMutableData<long long int>();
+
+    size_t sample_ids_len = decoder_outs[0].GetTensorTypeAndShapeInfo().GetElementCount();
+    cout << "[";
+    for (size_t i = 0; i < sample_ids_len; i++) {
+        cout << sample_ids[i] << ",";
+    }
+    cout << "]"; 
 }
